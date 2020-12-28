61a15ab679c558206617cea7f4dafdb5b9a02010
Neil Warner <neil.warner@tasconline.com>
2020-12-28 13:51:41 -0600

    Espresso test by the recorder shows 100% coverage fro hello world

---
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val textView = onView(
+            allOf(
+                withText("Hello World!"),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Hello World!")))
+    }
+}
